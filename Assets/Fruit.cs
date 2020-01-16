5d8a76b35c8ef717d50105cfeb66ac5652c77b0c
DAVICORREIA21 <davicorreia21@gmail.com>
2020-01-16 01:47:03 -0300

    Fruit config add fisica e começo do Script

---
+++ Assets/Fruit.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Fruit : MonoBehaviour
+{
+    public float force = 10f;
+
+    private Rigidbody2D rig;
+
+    void Start()
+    {
+        rig = GetComponent<Rigidbody2D>();
+        rig.AddForce(transform.up * force, ForceMode2D.Impulse);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
