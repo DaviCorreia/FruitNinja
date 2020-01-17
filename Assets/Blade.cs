1dc454ef0df8216ec7f224d8dbef3b31407624c8
DAVICORREIA21 <davicorreia21@gmail.com>
2020-01-17 01:29:41 -0300

    lamina add e Script lamina add

---
+++ Assets/Blade.cs
@@ -0,0 +1,65 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Blade : MonoBehaviour
+{
+    private Rigidbody2D rig;
+    private CircleCollider2D CircleColl;
+    public bool isCutting;
+    private Camera cam;
+    private Vector2 LastPos;
+    public float VelocityCut;
+
+    void Start()
+    {
+        cam = Camera.main;
+        rig = GetComponent<Rigidbody2D>();
+        CircleColl = GetComponent<CircleCollider2D>();
+    }
+
+    void StartCut()
+    {
+        isCutting = true;
+        CircleColl.enabled = true;
+        LastPos = cam.ScreenToWorldPoint(Input.mousePosition);
+    }
+    void StopCut()
+    {
+        isCutting = false;
+        CircleColl.enabled = false;
+    }
+    void Update()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            StartCut();
+        }
+        else if (Input.GetMouseButtonUp(0))
+        {
+            StopCut();
+        }
+        if (isCutting)
+        {
+            UpdateCut();
+        }
+    }
+
+    void UpdateCut()
+    {
+        Vector2 newPos = cam.ScreenToWorldPoint(Input.mousePosition);
+        rig.position = newPos;
+
+        float velocity = (newPos - LastPos).magnitude * Time.deltaTime;
+
+        if (velocity > VelocityCut)
+        {
+            CircleColl.enabled = true;
+        }
+        else
+        {
+            CircleColl.enabled = false;
+        }
+        LastPos = newPos;
+    }
+}
